7318af248f2ce13f1ef92437d7501a1b90c9239e
Sam Maaß <sam-github@maass.co>
2018-01-28 01:19:47 +0100

    add airport endpoint

---
+++ src/model/airportModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const airportSchema = new Schema({
+  "id": String,
+  "name": String,
+  "city": String,
+  "country": String,
+  "iata": String,
+  "icao": String,
+  "latitude": String,
+  "longitude": String,
+  "altitude": String,
+  "timezone": String,
+  "dst": String,
+  "tz": String
+});
+
+const AirportModel = mongoose.model('airport', airportSchema);
+
+module.exports = AirportModel;
