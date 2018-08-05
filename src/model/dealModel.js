4845524775c9a7099a51544b183fbc29990f01cc
Sam <sam@pop-os.localdomain>
2018-08-05 20:18:21 +0200

    mod: adjust deal model

--- src/model/dealModel.js
+++ src/model/dealModel.js
@@ -3,15 +3,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const dealSchema = new Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  destinations: [String],
-  origins: [String],
-  startDate: Date,
-  endDate: Date,
-  price: Number,
-  link: String,
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   createdAt: Date,
-  updatedAt: Date
+  updatedAt: Date,
+  title: String,
+  minPrice: Number,
+  origins: [String],
+  destinations: [String],
+  firstDeparture: Date,
+  lastReturn: Date,
+  exampleFlights: [
+    {
+      link: String,
+      price: Number,
+      inOrigin: String,
+      inDestination: String,
+      inDate: Date,
+      inCarriers: String,
+      outOrigin: String,
+      outDestination: String,
+      outDate: Date,
+      outCarriers: String
+    }
+  ]
 });
 
 const DealModel = mongoose.model('Deal', dealSchema);
