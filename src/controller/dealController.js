925288472426258dafe9bc6ad0cf0eb81f32ece3
Sam <sam@pop-os.localdomain>
2018-08-08 13:19:16 +0200

    feat: improve matching

--- src/controller/dealController.js
+++ src/controller/dealController.js
@@ -1,6 +1,8 @@
 const DealModel = require('../model/dealModel');
 const UserModel = require('../model/userModel');
 const FligthModel = require('../model/flightModel');
+const FlightController = require('./flightController');
+const moment = require('moment');
 
 module.exports = {
   get: async (req, res) => {
@@ -19,14 +21,8 @@ module.exports = {
     const user = await UserModel.findById(req.user._id);
     if (user) {
       const flightIds = await insertFlights(req.body.exampleFlights);
-      const deal = new DealModel({
-        ...req.body,
-        exampleFlights: flightIds,
-        user,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      });
-      await deal.save();
+      const deal = await insertDeal(req.body, flightIds, user);
+      FlightController._matchFlightsWithTrips(flightIds);
       res.status(200).json(deal);
     } else {
       res.status(500).json({ error: 'Unable to save deal' });
@@ -34,8 +30,31 @@ module.exports = {
   }
 };
 
+async function insertDeal(reqBody, flightIds, user) {
+  const deal = new DealModel({
+    ...reqBody,
+    exampleFlights: flightIds,
+    user,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  });
+  await deal.save();
+  return deal;
+}
+
 async function insertFlights(exampleFlights) {
-  const flights = await FligthModel.insertMany(exampleFlights);
+  const flightsWithDuration = exampleFlights.map(flight => {
+    return {
+      ...flight,
+      duration: moment(flight.inDate, 'YYYY-MM-DD').diff(
+        moment(flight.outDate, 'YYYY-MM-DD'),
+        'days'
+      )
+    };
+  });
+  console.log(flightsWithDuration);
+
+  const flights = await FligthModel.insertMany(flightsWithDuration);
   const flightIds = flights.map(flight => flight._id);
   return flightIds;
 }
