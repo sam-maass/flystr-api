5e54f8c01d3a74715439835bb3566a02d342c6e3
Sam <sam@pop-os.localdomain>
2018-11-07 13:28:46 +0100

    feat: better logging

--- src/utils/flights/removeOutdatedFlights.js
+++ src/utils/flights/removeOutdatedFlights.js
@@ -1,10 +1,14 @@
 import FlightModel from '../../model/flightModel';
 import { removeFlightById } from './removeFlightById';
 export async function removeOutdatedFlights() {
+  console.log('removing outdated flights ...');
+
   const flights = await FlightModel.find({
     outDate: { $lte: new Date() },
     removed: { $ne: true }
   });
+  console.log(`${flights.length}flights removed`);
+
   const flightIds = flights.map(f => f._id);
   flightIds.forEach(removeFlightById);
 }
