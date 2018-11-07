a5b0eaf13a0d0ad00aeabc45d0623094002787c9
Sam <sam@pop-os.localdomain>
2018-11-08 00:07:16 +0100

    fix: crawler url

--- src/utils/skyscannerCrawler.js
+++ src/utils/skyscannerCrawler.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
 export const crawler = Axios.create({
-  baseURL: 'http://localhost:3300'
-  //  baseURL: 'web://flystr-crawler:3300'
+  // baseURL: 'http://localhost:3300'
+  baseURL: 'web://flystr-crawler:3300'
 });
