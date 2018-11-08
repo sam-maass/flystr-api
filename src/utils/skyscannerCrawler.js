57a018060eab860cab90268e76780fb4ad749175
Sam <sam@pop-os.localdomain>
2018-11-08 12:06:35 +0100

    feat: use crawler url from .env

--- src/utils/skyscannerCrawler.js
+++ src/utils/skyscannerCrawler.js
@@ -1,6 +1,5 @@
 import Axios from 'axios';
 
 export const crawler = Axios.create({
-  // baseURL: 'http://localhost:3300'
-  baseURL: 'web://flystr-crawler:3300'
+  baseURL: process.env.CRAWLER_URL
 });
