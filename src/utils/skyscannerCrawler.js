d5dbd811d030cb1926368c6f5a4cae86c7f0e21f
Sam <sam@pop-os.localdomain>
2018-11-08 13:58:11 +0100

    debug prod

--- src/utils/skyscannerCrawler.js
+++ src/utils/skyscannerCrawler.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
-console.log(`CRAWLER_URL: ${process.env.CRAWLER_URL}`);
 const baseURL = process.env.CRAWLER_URL;
+console.log(`CRAWLER_URL: ${baseURL}`);
 
 export const crawler = Axios.create({
   baseURL
