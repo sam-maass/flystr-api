293824710e7e333c8baa949237eb4654af93867f
Sam <sam@pop-os.localdomain>
2018-11-08 13:49:13 +0100

    fix: crawler baseURL

--- src/utils/skyscannerCrawler.js
+++ src/utils/skyscannerCrawler.js
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 console.log(`CRAWLER_URL: ${process.env.CRAWLER_URL}`);
+const baseURL = process.env.CRAWLER_URL;
 
 export const crawler = Axios.create({
-  baseURL: process.env.CRAWLER_URL
+  baseURL
 });
