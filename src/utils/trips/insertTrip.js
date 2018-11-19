ff69be99c986ddc3a6eff2ada5e69e95163a2562
Sam <sam@pop-os.localdomain>
2018-11-19 22:46:20 +0100

    feat: get min price on trip creation

--- src/utils/trips/insertTrip.js
+++ src/utils/trips/insertTrip.js
@@ -5,10 +5,15 @@ import { crawler } from '../skyscannerCrawler';
 
 export async function insertTrip(tripData, user) {
   const matchingFlights = await findMatchingFlights(tripData);
+  const minPrice = Math.min(
+    ...matchingFlights.map(f => f.price).filter(v => v >= 0)
+  );
   const trip = new TripModel({
     ...tripData,
     user,
-    matchingFlights
+    matchingFlights,
+    minPrice,
+    currency: 'EUR'
   });
   await trip.save();
   crawler(`/crawl/trips/${trip._id}`);
