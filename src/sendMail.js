5af910ef1ce6e17699735c4e2e6f0539c7c7fe4a
Sam <sam@pop-os.localdomain>
2018-11-26 14:57:09 +0100

    feat: add email notifications

--- src/sendMail.js
+++ src/sendMail.js
@@ -1,25 +1,23 @@
-import sgMail from '@sendgrid/mail';
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
+import { mailer } from './mailer';
 const sendWelcomeEmail = function(email) {
   const msg = {
     to: email,
     from: 'sam@flystr.com',
     templateId: 'd-4150aec3e9b44cc6a1219da5616e4301'
   };
-  sgMail.send(msg);
+  mailer.send(msg);
 };
 
 const sendSignupEmail = function(email) {
   const msg = {
     to: email,
-    from: 'flystr@flystr.com',
+    from: 'Flystr.com <flystr@flystr.com>',
     templateId: 'd-de80ce5dbcec4b3ebd9a3c598e60f0c7',
     dynamic_template_data: {
       email
     }
   };
-  sgMail.send(msg);
+  mailer.send(msg);
 };
 
 module.exports = { sendWelcomeEmail, sendSignupEmail };
