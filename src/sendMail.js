89c9d0b047719991ed2b50887428a73f9410ddfd
Sam <sam@pop-os.localdomain>
2018-12-03 22:19:18 +0100

    update email address

--- src/sendMail.js
+++ src/sendMail.js
@@ -2,7 +2,7 @@ import { mailer } from './mailer';
 const sendWelcomeEmail = function(email) {
   const msg = {
     to: email,
-    from: 'sam@flystr.com',
+    from: 'sam@tripfixed.com',
     templateId: 'd-4150aec3e9b44cc6a1219da5616e4301'
   };
   mailer.send(msg);
@@ -11,7 +11,7 @@ const sendWelcomeEmail = function(email) {
 const sendSignupEmail = function(email) {
   const msg = {
     to: email,
-    from: 'Flystr.com <flystr@flystr.com>',
+    from: 'Tripfixed.com <flystr@tripfixed.com>',
     templateId: 'd-de80ce5dbcec4b3ebd9a3c598e60f0c7',
     dynamic_template_data: {
       email
