2a0da816f8d6152d95e512a6329118a409c8cf2b
Your Name <you@example.com>
2018-12-18 21:06:16 +0100

    feat: move schedule to crawler

--- src/jobs.js
+++
@@ -1,7 +0,0 @@
-import { crawler } from './utils/skyscannerCrawler';
-
-// Update Flights every 3 hours
-setInterval(() => {
-  crawler('/crawl/trips/all');
-  crawler('/crawl/deals/all');
-}, 6 * 60 * 60 * 1000);
